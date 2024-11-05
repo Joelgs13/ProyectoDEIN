2eab51e8555a373ff0d4b50cd309bed8b1b0ff73
Joelgs13 <050213jgonzalez@gmail.com>
2024-11-05 18:24:08 +0100

    Pantalla principal dibujada

---
+++ src/main/java/com/example/olimpiadas/controller/ListaDeAeropuertosController.java
@@ -0,0 +1,181 @@
+package com.example.ejerciciom;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ListaDeAeropuertosController {
+
+    @FXML
+    private MenuItem miAniadirDeporte;
+
+    @FXML
+    private MenuItem miAniadirDeportista;
+
+    @FXML
+    private MenuItem miAniadirEquipo;
+
+    @FXML
+    private MenuItem miAniadirEvento;
+
+    @FXML
+    private MenuItem miAniadirOlimpiada;
+
+    @FXML
+    private MenuItem miAniadirParticipacion;
+
+    @FXML
+    private MenuItem miBorrarDeporte;
+
+    @FXML
+    private MenuItem miBorrarDeportista;
+
+    @FXML
+    private MenuItem miBorrarEquipo;
+
+    @FXML
+    private MenuItem miBorrarEvento;
+
+    @FXML
+    private MenuItem miBorrarOlimpiada;
+
+    @FXML
+    private MenuItem miBorrarParticipacion;
+
+    @FXML
+    private MenuItem miEditarDeporte;
+
+    @FXML
+    private MenuItem miEditarDeportista;
+
+    @FXML
+    private MenuItem miEditarEquipo;
+
+    @FXML
+    private MenuItem miEditarEvento;
+
+    @FXML
+    private MenuItem miEditarOlimpiada;
+
+    @FXML
+    private MenuItem miEditarParticipacion;
+
+    @FXML
+    private TableView<?> tabla;
+
+    @FXML
+    private TextField tfNombre;
+
+    @FXML
+    void aniadirDeporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirDeportista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirEquipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirEvento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirOlimpiada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirParticipacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarAeropuerto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarDeporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarDeportista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarEquipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarEvento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarOlimpiada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarParticipacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cambiarDeTabla(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarAeropuerto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarDeporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarDeportista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarEquipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarEvento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarOlimpiada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarParticipacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filtrar(ActionEvent event) {
+
+    }
+
+}
