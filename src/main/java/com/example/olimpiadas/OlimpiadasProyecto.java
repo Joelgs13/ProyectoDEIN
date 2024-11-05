443cdf5288a444e0cb07680b6dd2ac46a7961662
Joelgs13 <050213jgonzalez@gmail.com>
2024-11-05 18:41:50 +0100

    estilos puestos e icono a la ventana principal

--- src/main/java/com/example/olimpiadas/OlimpiadasProyecto.java
+++ src/main/java/com/example/olimpiadas/OlimpiadasProyecto.java
@@ -3,6 +3,7 @@ package com.example.olimpiadas;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,9 @@ public class OlimpiadasProyecto extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(OlimpiadasProyecto.class.getResource("/com/example/olimpiadas/fxml/pantalla_principal.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/olimpiadas/img/iconitoOlimpiadas.png")));
         stage.setTitle("Olimpiadas!");
         stage.setScene(scene);
         stage.show();
